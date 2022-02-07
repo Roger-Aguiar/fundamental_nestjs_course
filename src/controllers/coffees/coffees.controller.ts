fa88a08fd58492e59fa62f0a0e66579e7004f836
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-02-07 08:11:08 -0300

    It creates the basic CRUD.

--- src/controllers/coffees/coffees.controller.ts
+++ src/controllers/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -6,4 +6,25 @@ export class CoffeesController {
     findAll(): string{
         return "This action returns all coffees!";
     }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string{
+        return `This action returns the coffee number ${id}`;
+    }
+
+    @Post()
+    create(@Body() body){
+        return body;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() body){
+        return `This action updates coffee number ${id}.`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string){
+        return `This action deletes the coffee number ${id}`;
+    }
+    //Next video lesson: Implement Pagination with Query Parameters
 }
