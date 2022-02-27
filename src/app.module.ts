27aae07790cdc1665774c0cde569caa716b9aa02
Roger <roger.silvaaguiar.com.br>
2022-02-27 17:01:47 -0300

    It creates the .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './modules/coffees/coffees.module';
 import { CoffeeRatingModule } from './modules/coffee-rating/coffee-rating.module';
 import { CoffeeRatingService } from './service/coffee-rating/coffee-rating.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule,
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '983453069',
-      database: 'coffees_database',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [],
       autoLoadEntities: true,
       synchronize: true,//Remember to enable this property to "false" when putting the application in production.
